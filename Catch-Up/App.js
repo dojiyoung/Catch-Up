e10490284f96a3079079b5549deabd9fdb855f6e
dojiyoung <84650928+dojiyoung@users.noreply.github.com>
2022-09-26 08:51:56 +1300

    add review card

--- Catch-Up/App.js
+++ Catch-Up/App.js
@@ -3,8 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import ReviewScreen from './screens/ReviewScreen/ReviewScreen';
 
 export default function App() {
+  const review = {
+    userName: "Jiyoung Do",
+    location: "@ Sunny side Coffee - dongdaemungu ",
+    reviewText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Phasellus eget purus sit amet sem convallis aliquet congue ac purus.
+     Maecenas non odio arcu. Vivamus feugiat at nisl sed rutrum. 
+     Maecenas quis facilisis mauris. Vivamus in turpis nisl. 
+     Fusce nec justo velit. 
+     Maecenas in arcu finibus, 
+     feugiat augue at, vestibulum sapien.`
+}
+
   return (
-    <ReviewScreen/>
+    <ReviewScreen review={review}/>
 
   );
 }
--- Catch-Up/screens/ReviewScreen/ReviewScreen.js
+++ Catch-Up/screens/ReviewScreen/ReviewScreen.js
@@ -3,11 +3,42 @@ import { SafeAreaView, Text, View, StyleSheet, Image } from 'react-native';
 
 import { Card } from 'react-native-paper';
 
-const ReviewScreen = () => {
+
+
+const ReviewScreen = (props) => {
+    
+    const { review } = props;
+    // same as 
+    // const review = props.review;
+    const { userName, location, reviewText} = review;
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Image style={styles.image} source={{ uri: 'https://i.ibb.co/Jdz0XJB/review1.png' }} />
+        
+
+        <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={{ uri: 'https://picsum.photos/10' }} />
+        <Text>
+            { userName }
+            {"\n"}
+            {"\n"}
+            {"\n"}
+        </Text>
+        </View>
+        <Text>
+            { location }
+            {"\n"}
+            {"\n"}
+        </Text>
+        <Text>
+            { reviewText }
+            {"\n"}
+        </Text>
+        <Image style={styles.image1} source={{ uri: 'https://i.ibb.co/vYy2JwS/image-1.png' }} />
+
+
+        {/* <Image style={styles.image} source={{ uri: 'https://i.ibb.co/Jdz0XJB/review1.png' }} /> */}
       </View>
     </SafeAreaView>
   );
@@ -17,7 +48,7 @@ export default ReviewScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+
     justifyContent: 'center',
     padding: 20,
     backgroundColor: '#ecf0f1',
@@ -26,10 +57,20 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 500
   },
-  paragraph: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 20
+  image1: {
+    width: "100%",
+    height: 200
+  },
+  logoContainer: {
+    flexDirection: "row"
+  },
+  logo: {
+    width: 50,
+    height: 50
+  },
+  card: {
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+    transition: "0.3s",
+    borderRadius: "5px"
   }
 });
